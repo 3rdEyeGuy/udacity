4f934909ec7bfdea2c7d59f0a62d26f3e1370583
Octavio <Oasosaf@gmail.com>
2017-07-25 11:28:28 -0500

    add probability dist after recursive measurements

--- AI4Robotics/l1.ex1.py
+++ AI4Robotics/l1.ex1.py
@@ -4,20 +4,20 @@
 
 p=[0.2, 0.2, 0.2, 0.2, 0.2]
 world=['green', 'red', 'red', 'green', 'green']
-measure = ['red','green']
+measurements = ['red','green']
 pHit = 0.6
 pMiss = 0.2
-def sense(p, measure):
+def sense(p, Z):
     q=[]
     for i in range(len(p)):
-        hit = (measure == world[i])
+        hit = (Z == world[i])
         q.append(p[i] * (hit * pHit + (1-hit) * pMiss))
-    norm = sum(q)
+    s = sum(q)
     for i in range(len(q)):
-        q[i] = q[i] / norm
+        q[i] = q[i] / s
     return q
 
-for i in measure:
-    print(i, 'probability:')
-    print(sense(p,i))
+for i in range(len(measurements)):
+    p = sense(p,measurements[i])
+print(p)
     

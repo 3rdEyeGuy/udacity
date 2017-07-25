3c8348cfd47698b38886e16f590ef3edfadf8843
Octavio <Oasosaf@gmail.com>
2017-07-25 11:07:28 -0500

    add probability dist for multiple measurements

--- AI4Robotics/l1.ex1.py
+++ AI4Robotics/l1.ex1.py
@@ -4,20 +4,20 @@
 
 p=[0.2, 0.2, 0.2, 0.2, 0.2]
 world=['green', 'red', 'red', 'green', 'green']
-measurements = ['red','green']
+measure = ['red','green']
 pHit = 0.6
 pMiss = 0.2
-
-def sense(p, measurements):
+def sense(p, measure):
     q=[]
     for i in range(len(p)):
-        hit = (measurements == world[i])
+        hit = (measure == world[i])
         q.append(p[i] * (hit * pHit + (1-hit) * pMiss))
     norm = sum(q)
     for i in range(len(q)):
         q[i] = q[i] / norm
     return q
 
-print(sense(p,measurements))
-
-
+for i in measure:
+    print(i, 'probability:')
+    print(sense(p,i))
+    

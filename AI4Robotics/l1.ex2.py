fb102148e40f0232e1f356d7ce00729d56d8137b
Octavio <Oasosaf@gmail.com>
2017-07-26 08:34:10 -0500

    negative index of a list test

--- AI4Robotics/l1.ex2.py
+++ AI4Robotics/l1.ex2.py
@@ -2,7 +2,8 @@
 #q, shifted to the right by U units. If U=0, q should 
 #be the same as p.
 
-p=[0, 1, 0, 0, 0]
+p=[0, 1, 2, 3, 4]
+print('test',p[0],p[-1],p[-2],p[-3], p[-4])
 world=['green', 'red', 'red', 'green', 'green']
 measurements = ['red', 'green']
 pHit = 0.6

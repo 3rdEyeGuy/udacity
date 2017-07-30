2722fec91e6e73ea0e6e93207096dc430e6b623e
Octavio <Oasosaf@gmail.com>
2017-07-30 13:55:08 -0500

    official answer

--- AI4Robotics/l1.ex3.py
+++ AI4Robotics/l1.ex3.py
@@ -20,9 +20,11 @@ def sense(p, Z):
 def move(p, U):
     q = []
     for i in range(len(p)):
-        q.append(p[(i-U)%len(p)])
+        s = pExact * (p[(i-U)%len(p)])
+        s = s + pOvershoot * (p[(i-U-1)%len(p)])
+        s = s + pUndershoot * (p[(i-U+1)%len(p)])
+        q.append(s)
     return q
     
 
 print(move(p, 1))
-print(sense(p, measurements))

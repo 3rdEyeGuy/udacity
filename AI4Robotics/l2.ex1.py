1c69f541ffd4d5b4e19f5d4476f0586f183ce21f
Octavio <Oasosaf@gmail.com>
2017-08-06 19:12:15 -0500

    remove initializations (inits) before calling localization()

--- AI4Robotics/l2.ex1.py
+++ AI4Robotics/l2.ex1.py
@@ -83,9 +83,6 @@ def sense(p, colors, measurements, sensor_wrong):
     return q
 
 def localize(colors,measurements,motions,sensor_right,p_move):
-    p_move = p_move
-    sensor_right = sensor_right
-
     # initializes p to a uniform distribution over a grid of the same dimensions as colors
     pinit = 1.0 / float(len(colors)) / float(len(colors[0]))
     # uniform distribution of 0.005 probability in each cell
@@ -103,23 +100,6 @@ def show(p):
     rows = ['[' + ','.join(map(lambda x: '{0:.5f}'.format(x),r)) + ']' for r in p]
     print('[' + ',\n '.join(rows) + ']')
     
-#############################################################
-# For the following test case, your output should be 
-# [[0.01105, 0.02464, 0.06799, 0.04472, 0.02465],
-#  [0.00715, 0.01017, 0.08696, 0.07988, 0.00935],
-#  [0.00739, 0.00894, 0.11272, 0.35350, 0.04065],
-#  [0.00910, 0.00715, 0.01434, 0.04313, 0.03642]]
-# (within a tolerance of +/- 0.001 for each entry)
-
-colors = [['R','G','G','R','R'],
-          ['R','R','G','R','R'],
-          ['R','R','G','G','R'],
-          ['R','R','R','R','R']]
-measurements = ['G','G','G','G','G']
-motions = [[0,0],[0,1],[1,0],[1,0],[0,1]]
-sensor_right = 0.7
-p_move = 0.8
-
 p = localize(colors,measurements,motions,sensor_right, p_move)
 
     
